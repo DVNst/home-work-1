d61a9e796ca09a516c5a340ee7764d811ef524c7
DVNst <dvn-studio@mail.ru>
2021-12-10 17:08:45 +0300

    tutorial-7 complete

--- src/tutorial-7/App.js
+++ src/tutorial-7/App.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Box, Grid } from "@mui/material";
 
 import "./styles.css";
 
 function App() {
-  const { handleSubmit, register, formState } = useForm();
+  const { handleSubmit, register, formState, reset } = useForm();
 
-  const onSubmit = (values) => console.log("ФОРМА!", values);
-
-  console.log(formState.errors);
+  const onSubmit = (values) => {
+    console.log("ФОРМА!", values);
+    reset();
+  };
 
   return (
     <div className="App">
-      <div className="row">
+      <Box
+        component="form"
+        noValidate
+        autoComplete="off"
+        sx={{ my: 2, mx: "auto", p: 2, maxWidth: 400, flexGrow: 1, '& .MuiTextField-root': { my: 1 } }}
+      >
         <TextField
           name="firstName"
           label="Имя"
           {...register("firstName", {
-            validate: (value) => value !== "admin" || "Nice try!"
+            validate: (value) => value !== "admin" || "Nice try!",
           })}
-          helperText={formState.errors.firstName && formState.errors.firstName.message}
+          helperText={
+            formState.errors.firstName && formState.errors.firstName.message
+          }
           error={!!formState.errors.firstName}
           fullWidth
         />
@@ -28,20 +36,21 @@ function App() {
           name="lastName"
           label="Фамилия"
           {...register("lastName", {
-            required: "Это обязательное поле!"
+            required: "Это обязательное поле!",
           })}
-          helperText={formState.errors.lastName && formState.errors.lastName.message}
+          helperText={
+            formState.errors.lastName && formState.errors.lastName.message
+          }
           error={!!formState.errors.lastName}
           fullWidth
         />
-      </div>
-      <div className="row">
+
         <TextField
           {...register("email", {
             pattern: {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
-              message: "Это неверная почта!"
-            }
+              message: "Это неверная почта!",
+            },
           })}
           helperText={formState.errors.email && formState.errors.email.message}
           error={!!formState.errors.email}
@@ -52,28 +61,40 @@ function App() {
         />
         <TextField
           {...register("password", {
-            required: "Это обязательное поле!"
+            required: "Это обязательное поле!",
           })}
-          helperText={formState.errors.password && formState.errors.password.message}
+          helperText={
+            formState.errors.password && formState.errors.password.message
+          }
           error={!!formState.errors.password}
           name="password"
           type="password"
           label="Пароль"
           fullWidth
         />
-      </div>
-      <div className="row">
-        <TextField name="about" label="Обо мне" fullWidth />
-      </div>
-      <br />
-      <div className="row">
-        <Button onClick={handleSubmit(onSubmit)} variant="contained" color="primary">
-          Зарегистрироваться
-        </Button>
-        <Button variant="contained" color="secondary">
-          Очистить
-        </Button>
-      </div>
+
+        <TextField
+          name="about"
+          label="Обо мне"
+          fullWidth
+          multiline
+          rows={4}
+        />
+        
+        <div className='row'>
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            variant="contained"
+            color="primary"
+          >
+            Зарегистрироваться
+          </Button>
+
+          <Button variant="contained" color="secondary" onClick={() => reset()}>
+            Очистить
+          </Button>
+        </div>
+      </Box>
     </div>
   );
 }
