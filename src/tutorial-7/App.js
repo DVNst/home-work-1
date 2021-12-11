fe5cbc8bae0794f8a01b5a3dd7d18ed338e15b0f
DVNst <dvn-studio@mail.ru>
2021-12-11 07:38:15 +0300

    tutorial-7 add validation with YUP

--- src/tutorial-7/App.js
+++ src/tutorial-7/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import { Button, TextField, Box, Grid } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import "./styles.css";
 
+const schema = yup.object({
+  firstName: yup.string().required('Это обязательное поле').min(2, 'минимум 2 символа'),
+  lastName: yup.string(),
+  email: yup.string().email('Введите корректный e-mail').required('Это обязательное поле'),
+  password: yup.string().required('Это обязательное поле').min(6, 'минимум 6 символов').matches(/[a-zA-Z]/, 'Только латинские буквы'),
+}).required();
+
 function App() {
-  const { handleSubmit, register, formState, reset } = useForm();
+  const { handleSubmit, register, formState, reset } = useForm({resolver: yupResolver(schema)});
 
   const onSubmit = (values) => {
     console.log("ФОРМА!", values);
@@ -21,59 +30,46 @@ function App() {
         sx={{ my: 2, mx: "auto", p: 2, maxWidth: 400, flexGrow: 1, '& .MuiTextField-root': { my: 1 } }}
       >
         <TextField
+          {...register('firstName')}
           name="firstName"
           label="Имя"
-          {...register("firstName", {
-            validate: (value) => value !== "admin" || "Nice try!",
-          })}
-          helperText={
-            formState.errors.firstName && formState.errors.firstName.message
-          }
+          defaultValue=''
+          helperText={formState.errors.firstName && formState.errors.firstName.message}
           error={!!formState.errors.firstName}
           fullWidth
         />
         <TextField
+          {...register('lastName')}
           name="lastName"
           label="Фамилия"
-          {...register("lastName", {
-            required: "Это обязательное поле!",
-          })}
-          helperText={
-            formState.errors.lastName && formState.errors.lastName.message
-          }
+          defaultValue=''
+          helperText={formState.errors.lastName && formState.errors.lastName.message}
           error={!!formState.errors.lastName}
           fullWidth
         />
 
         <TextField
-          {...register("email", {
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9._%+-]+\.[A-Z]{2,}$/i,
-              message: "Это неверная почта!",
-            },
-          })}
+          {...register('email')}
+          name="email"
+          label="E-mail"
+          defaultValue=''
           helperText={formState.errors.email && formState.errors.email.message}
           error={!!formState.errors.email}
-          name="email"
-          label="E-Mail"
-          defaultValue=""
           fullWidth
         />
         <TextField
-          {...register("password", {
-            required: "Это обязательное поле!",
-          })}
-          helperText={
-            formState.errors.password && formState.errors.password.message
-          }
-          error={!!formState.errors.password}
-          name="password"
+          {...register('password')}
           type="password"
           label="Пароль"
+          name="password"
+          defaultValue=''
+          helperText={formState.errors.password && formState.errors.password.message}
+          error={!!formState.errors.password}
           fullWidth
         />
 
         <TextField
+          {...register('about')}
           name="about"
           label="Обо мне"
           fullWidth
@@ -83,14 +79,17 @@ function App() {
         
         <div className='row'>
           <Button
-            onClick={handleSubmit(onSubmit)}
             variant="contained"
             color="primary"
+            onClick={handleSubmit(onSubmit)}
           >
             Зарегистрироваться
           </Button>
 
-          <Button variant="contained" color="secondary" onClick={() => reset()}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => reset()}>
             Очистить
           </Button>
         </div>
