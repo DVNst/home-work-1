b3be457ac1b4d0279c4c19c666dcb34e2d784e55
Дедюрин Виталий Николаевич <DedyurinVN@moscow.gbumac.ru>
2021-12-14 16:40:13 +0300

    tutorial-9 complete

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -1,17 +1,52 @@
-import React from "react";
-import UserInfo from "./components/user-info/user-info";
+import React from 'react';
+import UserInfo from './components/user-info/user-info';
 
-import "./styles.css";
+import axios from 'axios';
+
+import './styles.css';
 
 function App() {
+  const [userInfo, setUserInfo] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
+  
+  const getUserInfo = async (userName) => {
+    await axios.get(`https://api.github.com/users/${userName}`)
+      .then(({data}) => {
+        setIsError(false);
+        setIsLoading(false);
+        setUserInfo(data);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        if (err.response && err.response.status === 404) {
+          setIsError(`Пользователь ${userName} не найден`);
+        }
+        else {
+          setIsError('Сервер github не отвечает, попробуйте позже');
+        }
+      });
+  };
+
+  const clickSearch = (e) => {
+    e.preventDefault();
+    if (e.target[0].value) {
+      // console.log('ЗАГРУЗКА', e.target[0].value);
+      setUserInfo({});
+      setIsLoading(true);
+      getUserInfo(e.target[0].value);
+    };
+  }
+
   return (
-    <div className="app" id="app">
-      <div className="app-container">
-        <form className="app-form">
-          <input type="text" className="app-input" placeholder="Укажите GitHub-аккаунт" />
-          <button className="app-form_btn">Найти</button>
+    <div className='app' id='app'>
+      <div className='app-container'>
+        <form className='app-form' onSubmit={clickSearch}>
+          <input type='text' className='app-input' placeholder='Укажите GitHub-аккаунт' />
+          <button className='app-form_btn' type='submit'>Найти</button>
         </form>
-        <UserInfo />
+        <UserInfo userInfo={userInfo} isLoading={isLoading} isError={isError}/>
+        {console.log('userInfo', userInfo)}
       </div>
     </div>
   );
--- src/tutorial-9/components/user-info/user-info.jsx
+++ src/tutorial-9/components/user-info/user-info.jsx
@@ -1,40 +1,60 @@
-const UserInfo = ({ users }) => {
+import Spinner from '../spinner/spinner';
+
+const UserInfo = ({ userInfo, isLoading, isError }) => {
+  const divisionThousands = (n) => {
+    if (n >= 1000000) {
+      return (`${(n / 1000000).toFixed(1)}кк`);
+    } else if (n >= 1000) {
+      return (`${(n / 1000).toFixed(1)}к`);
+    };
+    return n;
+  };
+
+  const public_repos = divisionThousands(userInfo.public_repos);
+  const followers = divisionThousands(userInfo.followers);
+  const following = divisionThousands(userInfo.following);
+
   return (
-    <div className="app-user">
-      <div className="app-user_info">
-        <div className="app-user_image">
-          <img src="./assets/img/avatar.png" alt=""/>
-        </div>
-        <div className="app-user_data">
-          <h1 className="app-user_name">
-            Archakov Dennis
-            <span>@archakov06</span>
-          </h1>
-          <p className="app-user_about">
-            Frontend Developer. UI Designer. JavaScript ❤️ ReactJS ⚛ React Native, NodeJS, PHP
-          </p>
-        </div>
-      </div>
-      <ul className="app-user_stats">
-        <li className="app-user_stats-item">
-          Репозитории
-          <span>124</span>
-        </li>
-        <li className="app-user_stats-item">
-          Подписчиков
-          <span>1.2к</span>
-        </li>
-        <li className="app-user_stats-item">
-          Звёзд
-          <span>506</span>
-        </li>
-      </ul>
-      <ul className="app-user_location">
-        <li className="app-user_location-item">Russia, Ingushetia, Nazran</li>
-        <li className="app-user_location-item">
-          <a href="http://archakov.im">http://archakov.im</a>
-        </li>
-      </ul>
+    <div className='app-user'>
+      {(Object.keys(userInfo).length > 0) && (
+        <>
+          <div className='app-user_info'>
+            <div className='app-user_image'>
+              <img src={userInfo.avatar_url} alt='' width='90px' height='90px' />
+            </div>
+            <div className='app-user_data'>
+              <h1 className='app-user_name'>
+                {userInfo.name}
+                <span><a href={userInfo.html_url} target='_blank' rel='noreferrer'>@{userInfo.login.toLowerCase()}</a></span>
+              </h1>
+              <p className='app-user_about'>
+                {userInfo.bio}
+              </p>
+            </div>
+          </div>
+          <ul className='app-user_stats'>
+            <li className='app-user_stats-item'>
+              Репозитории &nbsp;
+              <span>{public_repos}</span>
+            </li>
+            <li className='app-user_stats-item'>
+              Подписчиков &nbsp;
+              <span>{followers}</span>
+            </li>
+            <li className='app-user_stats-item'>
+              Подписан &nbsp;
+              <span>{following}</span>
+            </li>
+          </ul>
+          <ul className='app-user_location'>
+            <li className='app-user_location-item'>{userInfo.location}</li>
+            <li className='app-user_location-item'>
+              <a href={userInfo.blog}>{userInfo.blog}</a>
+            </li>
+          </ul>
+        </>)}
+        {isLoading && <Spinner />}
+        {isError && <p1>{isError}</p1>}
     </div>
   );
 };
