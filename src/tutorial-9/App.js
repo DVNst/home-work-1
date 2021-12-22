bb97a3adeb55be3c5d7b7b62a3e44a9af80c6e92
Дедюрин Виталий Николаевич <DedyurinVN@moscow.gbumac.ru>
2021-12-22 17:01:31 +0300

    tutorial-9 add pathname

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -6,23 +6,32 @@ import axios from 'axios';
 import './styles.css';
 
 function App() {
+  const { pathname } = window.location;
   const [userInfo, setUserInfo] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
   const [isError, setIsError] = React.useState(false);
-  
+
+  React.useEffect(() => {
+    if (pathname.startsWith('/login=') && pathname.slice(7) !== '') {
+      getUserInfo(pathname.slice(7));
+    }
+  }, []);
+
   const getUserInfo = (userName) => {
     setUserInfo(null);
     setIsLoading(true);
     axios.get(`https://api.github.com/users/${userName}`)
-      .then(({data}) => {
+      .then(({ data }) => {
         setIsError(false);
         setIsLoading(false);
         setUserInfo(data);
+        window.history.pushState('', '', `login=${userName}`);
       })
       .catch((err) => {
         setIsLoading(false);
         if (err.response && err.response.status === 404) {
           setIsError(`Пользователь ${userName} не найден`);
+          window.history.pushState('', '', `login=${userName}`);
         }
         else {
           setIsError('Сервер github не отвечает, попробуйте позже');
@@ -37,14 +46,18 @@ function App() {
     };
   }
 
+  const handleInputSearch = (e) => {
+    // console.log(e.target.value.trim());
+  }
+
   return (
     <div className='app' id='app'>
       <div className='app-container'>
         <form className='app-form' onSubmit={handleSubmitSearch}>
-          <input type='text' className='app-input' placeholder='Укажите GitHub-аккаунт' />
+          <input onChange={handleInputSearch} type='text' className='app-input' placeholder='Укажите GitHub-аккаунт' />
           <button className='app-form_btn' type='submit'>Найти</button>
         </form>
-        <UserInfo userInfo={userInfo} isLoading={isLoading} isError={isError}/>
+        <UserInfo userInfo={userInfo} isLoading={isLoading} isError={isError} />
       </div>
     </div>
   );
